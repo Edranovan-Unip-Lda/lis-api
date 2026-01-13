f92f23285080c315508cc46496a502939d13febc
Geovannio Frederico de Jesus Vinhas <geovanniovinhas@live.com>
2026-01-13 11:12:05 +0900

    Add method to search Suco by name (case-insensitive)
    Introduced findByNomeContainingIgnoreCase to SucoRepository to enable searching for Suco entities by name, ignoring case sensitivity.

--- src/main/java/tl/gov/mci/lis/repositories/endereco/SucoRepository.java
+++ src/main/java/tl/gov/mci/lis/repositories/endereco/SucoRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tl.gov.mci.lis.models.endereco.Suco;
 
+import java.util.List;
+
 @JaversSpringDataAuditable
 @RepositoryRestResource(collectionResourceRel = "sucos", path = "sucos")
 public interface SucoRepository extends JpaRepository<Suco, Long> {
+    List<Suco> findByNomeContainingIgnoreCase(String nome);
 }
